d35a57b5ceaf16e96cec2487bb9acc672d39abee
Tim Wu <tim7012@msn.com>
2016-06-13 18:59:11 +0800

    post+comment CRUD, with authentication

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,58 @@
+class CommentsController < ApplicationController
+
+before_action :set_post
+
+
+  def show
+
+  end
+
+
+  def new
+    @comment = @post.comments.new
+  end
+
+  def edit
+
+  end
+
+  def create
+    @comment = @post.comments.new(comment_params)
+    if @comment.save
+      redirect_to post_path(@post)
+    else
+      render :action => :new
+    end
+  end
+
+  def edit
+    @comment = @post.comments.find(params[:id])
+  end
+
+  def update
+    @comment = @post.comments.find(params[:id])
+    if @comment.udpate(comment_params)
+      redirect_to post_comments_path(@post)
+    else
+      render :action => :edit
+    end
+  end
+
+  def destroy
+    @comment = @post.comments.find(params[:id])
+    @comment.destroy
+
+    redirect_to post_comments_path(@post)
+  end
+
+  protected
+
+  def set_post
+    @post = Post.find(params[:post_id])
+  end
+
+  def comment_params
+    params.require(:comment).permit(:content)
+  end
+
+end
